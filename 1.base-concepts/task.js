11bf7793b215a940e2f910d50e99d3596e0d33c4
E Vi <metelevae@mail.ru>
2023-03-11 16:16:05 +0300

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let x1
+  let x2
+  const d = ((b * b) - 4 * a * c);
+
+ if (d > 0) {
+  x1 = (-b + Math.sqrt(d) )/(2*a);
+  x2 = (-b - Math.sqrt(d) )/(2*a);
+  arr = [x1, x2];
+}  else if (d == 0) {
+  x1 = (-b/(2*a));
+  arr = [x1];
+} else {
+  arr = [];
+}
+return arr;
 }
 
+
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+ 
+
+
+
+
+
+
+
+
+
 }

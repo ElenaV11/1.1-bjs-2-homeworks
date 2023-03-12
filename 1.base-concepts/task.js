0d2f749e1eae85585f94ee3ac6c967db8e7365b8
E Vi <metelevae@mail.ru>
2023-03-12 17:36:56 +0300

    Первое задание и второе

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let x1
-  let x2
+  
   const d = ((b * b) - 4 * a * c);
 
  if (d > 0) {
-  x1 = (-b + Math.sqrt(d) )/(2*a);
-  x2 = (-b - Math.sqrt(d) )/(2*a);
+  x1 = ((-b + Math.sqrt(d) )/(2*a));
+  x2 = ((-b - Math.sqrt(d) )/(2*a));
   arr = [x1, x2];
-}  else if (d == 0) {
+}  else if (d === 0) {
   x1 = (-b/(2*a));
   arr = [x1];
 } else {
@@ -22,14 +21,16 @@ return arr;
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
- 
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+   return false;
+ } else {
+   let interestRate, amountOfCredit, payment, sum;
+   interestRate = (percent / 100) / 12;
+   amountOfCredit = amount - contribution;
+   payment = amountOfCredit * (interestRate + (interestRate / (((1 + interestRate)^countMonths) - 1)));
+   sum = payment * countMonths;
 
-
-
-
-
-
-
-
-
-}
+   }
+   return sum;
+ }
+   

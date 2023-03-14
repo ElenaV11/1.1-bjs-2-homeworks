1bd5753e7d7b48e396a37e403d78095a705704f1
E Vi <metelevae@mail.ru>
2023-03-14 14:05:58 +0300

    Доработка дз

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,6 +3,8 @@ function solveEquation(a, b, c) {
   let arr = [];
   
   const d = ((b * b) - 4 * a * c);
+  let x1;
+  let x2;
 
  if (d > 0) {
   x1 = ((-b + Math.sqrt(d) )/(2*a));
@@ -21,10 +23,10 @@ return arr;
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let interestRate, amountOfCredit, payment, sum;
   if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
    return false;
  } else {
-   let interestRate, amountOfCredit, payment, sum;
    interestRate = (percent / 100) / 12;
    amountOfCredit = amount - contribution;
    payment = amountOfCredit * (interestRate + (interestRate / (((1 + interestRate)**countMonths) - 1)));

e2d54b283f6cf94404effc8552cc31e813701722
E Vi <metelevae@mail.ru>
2023-03-14 16:25:36 +0300

    Функции дз 1 готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,18 @@
-function getArrayParams(...arr) {
+function getArrayParams(...args) {
+ let min = Math.min (...args);
+ let max = Math.max (...args);
+ let sum = args.reduce((accumulator, currentValue) => accumulator + currentValue);
+ let averageValue = sum / args.length;
+ let avg = +averageValue.toFixed(2)
 
   return { min: min, max: max, avg: avg };
 }
+
+
+
+
+
+
 
 function summElementsWorker(...arr) {
 

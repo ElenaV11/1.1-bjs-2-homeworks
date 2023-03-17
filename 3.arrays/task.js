4e95b11874e6d8466ec239ebc0d681fab807b1b1
E Vi <metelevae@mail.ru>
2023-03-17 20:35:25 +0300

    первая задача

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
+    if(arr1.length != arr2.length) {
+        return false;
+    }
+      return arr1.every((item, i) => item === arr2[i]);
+  }
   
-}
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+
+
+  function getUsersNamesInAgeRange(users, gender) {
+  return users.filter(user => user.gender === gender)
 }

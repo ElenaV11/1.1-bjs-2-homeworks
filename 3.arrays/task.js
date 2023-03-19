cd1fcc3f419dc7cdfa392f7eff5db4a0e0959b0c
E Vi <metelevae@mail.ru>
2023-03-19 17:16:41 +0300

    Обе задачи выполнены

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -9,5 +9,5 @@ function compareArrays(arr1, arr2) {
 
 
   function getUsersNamesInAgeRange(users, gender) {
-  return users.filter(user => user.gender === gender)
+  return users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, arr) => acc + age / arr.length, 0);
 }

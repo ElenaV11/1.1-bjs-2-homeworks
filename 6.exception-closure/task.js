0a107b509a06580ef53fc7de332dbeb4b9b86010
E Vi <metelevae@mail.ru>
2023-04-08 19:20:39 +0300

    задачи выполнены

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,49 @@
-﻿
+﻿// задача 1
+function parseCount(value) {
+    if (isNaN(value)) {
+        throw new Error("Невалидное значение");
+    } else {
+        return Number.parseFloat;
+    }
+}
+function validateCount(value) {
+    try {
+        return parseCount;
+    } catch(error) {
+        return error;
+    }
+}
+
+// задача 2
+class Triangle {
+    constructor(a, b, c) {
+        if(a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+    get area() {
+        const p = this.perimeter;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return +area.toFixed(3);
+    }
+}
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+        };
+    }
+}

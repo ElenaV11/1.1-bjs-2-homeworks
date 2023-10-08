03a80b3f595906a03c3058adaad3d6029cdbb003
E Vi <metelevae@mail.ru>
2023-10-08 13:20:23 +0300

    задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,56 @@
 //Задача № 1
+const md5 = require('./js-md5.js');
+
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+    
+    function wrapper(...args) {
+        const hash = md5(args); 
+        let objectInCache = cache.find(item => item.hash === hash); 
+        if (objectInCache) { 
+            console.log("Из кеша: " + objectInCache.value);
+            return "Из кеша: " + objectInCache.value;
+        }
+    
+        let result = func(...args); 
+        cache.push({hash: hash, value: result}) ; 
+        if (cache.length > 5) { 
+            cache.shift(); 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+    }
+    return wrapper;
+    }
+
+module.exports = {
+  cachingDecoratorNew
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId = null;
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	function wrapper(...args) {
+		if (timeoutId === null) {
+			func(...args);
+			wrapper.count++;
+		}
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => {
+			timeoutId = true;
+			func(...args);
+			wrapper.count++;
+		}, delay);
+		wrapper.allCount++;
+	}
+	return wrapper;
+}
+
+module.exports = {
+  debounceDecoratorNew, 
 }
